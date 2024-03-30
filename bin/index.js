e080a5cc57946b904bf3e3a7193dbf6eaa951711
akii09 <akashpise588@gmail.com>
2024-03-30 17:06:59 +0530

    Update file paths and add console logs

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,15 @@
-#!/usr/bin/env node
-
-// Import needed packages
 import inquirer from 'inquirer';
 import fs from 'fs';
 import path, { dirname as pathDirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// __dirname in ES Module scope
 const __dirname = pathDirname(fileURLToPath(import.meta.url));
+console.log(__dirname, '__dirname');
 
-// Paths to templates
 const templates = {
-  "React": path.join(__dirname, 'templates', 'react'),
-  "Vue": path.join(__dirname, 'templates', 'vue'),
-  "Javascript": path.join(__dirname, 'templates', 'javascript'),
+    "React": path.join(__dirname, '..', 'templates', 'react'),
+    "Vue": path.join(__dirname, '..', 'templates', 'vue'),
+    "Javascript": path.join(__dirname, '..', 'templates', 'javascript'),
 };
 
 // Ask questions
@@ -34,18 +30,19 @@ inquirer
   .then((answers) => {
       // Create new directory and copy selected template
       fs.mkdirSync(answers.name);
+      console.log(templates[answers.template], 'PATH----')
       copy(answers.name, templates[answers.template]);
   });
 
 // Function to perform the copy
 function copy(dest, src) {
-   if (fs.lstatSync(src).isDirectory()) {
-      fs.mkdirSync(dest);
-      fs.readdirSync(src).forEach((child_item_name) => {
-        copy(path.join(dest, child_item_name),
-          path.join(src, child_item_name));
-       });
-   } else {
-       fs.writeFileSync(dest, fs.readFileSync(src));
-   }
-}
+    if (fs.lstatSync(src).isDirectory()) {
+       fs.mkdirSync(dest, { recursive: true });  // creates directory recursively
+       fs.readdirSync(src).forEach((child_item_name) => {
+         copy(path.join(dest, child_item_name),
+           path.join(src, child_item_name));
+        });
+    } else {
+        fs.writeFileSync(dest, fs.readFileSync(src));
+    }
+ }
